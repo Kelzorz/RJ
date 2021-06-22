d67418fa6363c2f9d13cd1c9887cdacaf708a8d6
Kelzorz <43789359+Kelzorz@users.noreply.github.com>
2021-06-22 11:08:15 -0500

    Don't Change Dates

--- Stored Procedures/UpdateEvents.sql
+++ Stored Procedures/UpdateEvents.sql
@@ -5,6 +5,6 @@ BEGIN
   FROM e_recuring_events WHERE id = arg1 ORDER BY ID DESC LIMIT 1;
     
     UPDATE e_events
-    SET title = @t_title, `description` = @t_description, cal_id = @t_cal_id, project_id = @t_project_id, `start` = @t_start, `end` = @t_end, starttime = @t_starttime, endtime = @t_endtime, event_owner = @t_event_owner, pgm_id = @t_pgm_id, event_user_id = @t_event_user_id, signups = @t_signups, display_signups = @t_display_signups, block_signups = @t_block_signups, event_location = @t_event_location, event_address = @t_event_address, event_url = @t_event_url, event_contact = @t_event_contact, event_file = @t_event_file, event_file_size = @t_event_file_size, `status` = @t_status, created = @t_created, updated = @t_updated
+    SET title = @t_title, `description` = @t_description, cal_id = @t_cal_id, project_id = @t_project_id, starttime = @t_starttime, endtime = @t_endtime, event_owner = @t_event_owner, pgm_id = @t_pgm_id, event_user_id = @t_event_user_id, signups = @t_signups, display_signups = @t_display_signups, block_signups = @t_block_signups, event_location = @t_event_location, event_address = @t_event_address, event_url = @t_event_url, event_contact = @t_event_contact, event_file = @t_event_file, event_file_size = @t_event_file_size, `status` = @t_status, created = @t_created, updated = @t_updated
     WHERE template_id = arg1;
 END
